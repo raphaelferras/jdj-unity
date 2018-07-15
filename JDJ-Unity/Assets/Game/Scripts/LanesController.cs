961b47251c2fceadbac627a8946dfa4eca19faa8
Raphael Ferras <raphaelferras@gmail.com>
2018-07-15 13:50:39 -0400

    Lanes created by code

---
+++ JDJ-Unity/Assets/Game/Scripts/LanesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanesController : MonoBehaviour {
+
+    public int lanesCount;
+
+    public GameObject lanePrefab;
+    public GameObject lanePrefab2;
+    public float startPos = -2.5f;
+    public float zposition = 25.5f;
+
+    // Use this for initialization
+    void Start () {
+        float x = startPos;
+        float delta = (startPos * -2)/(lanesCount-1);
+		for (int i =0; i < lanesCount; i++, x+= delta)
+        {
+            GameObject lane;
+            if(i %2 == 0) {
+                lane = Instantiate(lanePrefab, this.transform);
+            } else
+            {
+                lane = Instantiate(lanePrefab2, this.transform);
+            }
+            Vector3 pos = lane.transform.position;
+            pos.Set(x, pos.y, zposition);
+            lane.transform.position = pos;
+            Vector3 scale = lane.transform.localScale;
+            scale.Set(delta, scale.y, scale.z);
+            lane.transform.localScale = scale;
+        }
+    }
+
+}
