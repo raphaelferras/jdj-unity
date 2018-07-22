7e4cfae90ff4ac73f169b8cf280f304f019f808f
Raphael Ferras <raphaelferras@gmail.com>
2018-07-22 19:47:26 -0400

    first wall animation

---
+++ JDJ-Unity/Assets/_Game/Scripts/DestroyParticle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystem parts = GetComponent<ParticleSystem>();
+        Destroy(this.gameObject, parts.main.duration);
+    }
+}
