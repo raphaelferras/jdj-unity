c1d6c1a78e35099a7a690d2c05b6951a68dcabc5
Raphael Ferras <raphaelferras@gmail.com>
2018-08-06 15:29:52 -0400

    replace jelly icons

---
+++ JDJ-Unity/Assets/_Game/Scripts/LevelSelector/LevelOptmizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MapEnabledInterval
+{
+    public GameObject mapContainer;
+    public float startPosition;
+    public float endPosition;
+}
+
+public class LevelOptmizer : MonoBehaviour {
+
+    public List<MapEnabledInterval> mapEnable;
+
+	
+	// Update is called once per frame
+	void Update () {
+        CheckIfHaveToEnable();
+	}
+
+    private void CheckIfHaveToEnable()
+    {
+        float camPosition = CameraController.Instance.position;
+        foreach(MapEnabledInterval me in mapEnable)
+        {
+            if(camPosition > me.startPosition && camPosition < me.endPosition)
+            {
+                me.mapContainer.SetActive(true);
+            } else
+            {
+                me.mapContainer.SetActive(false);
+            }
+        }
+    }
+}
